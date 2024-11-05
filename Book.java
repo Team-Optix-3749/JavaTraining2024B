e3a76b14583a564a2962566f9357289362c2602c
SlushEE0 <neeladem2009@gmail.com>
2024-11-05 15:19:14 -0800

    initial

---
+++ Book.java
@@ -0,0 +1,11 @@
+package Day10;
+
+public class Book {
+    String name;
+    Genre genre;
+    String author;
+}
+
+enum Genre {
+    
+}
---
+++ Library.java
@@ -0,0 +1,11 @@
+package Day10;
+
+import java.util.ArrayList;
+
+public class Library {
+    ArrayList<Shelf> shelves = new ArrayList<Shelf>();
+}
+
+class Shelf {
+    
+}
